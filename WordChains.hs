c107ac2dbd2f248d6d8cfba620e4e146810cef6b
Steve Purcell <steve@sanityinc.com>
2014-08-31 16:34:55 +0100

    Pre-compute neighbours

--- WordChains.hs
+++ WordChains.hs
@@ -1,13 +1,16 @@
 {-# LANGUAGE OverloadedStrings #-}
 module WordChains (chains, readDict, standardDict) where
 
-import           Data.Set     (Set)
-import qualified Data.Set     as S
-import           Data.Text    (Text)
-import qualified Data.Text    as T
-import qualified Data.Text.IO as TIO
-import           Data.Tree    (Tree)
-import qualified Data.Tree    as Tr
+import           Control.Arrow   ((&&&))
+import qualified Data.Map        as M
+import qualified Data.Map.Strict as MS
+import           Data.Set        (Set)
+import qualified Data.Set        as S
+import           Data.Text       (Text)
+import qualified Data.Text       as T
+import qualified Data.Text.IO    as TIO
+import           Data.Tree       (Tree)
+import qualified Data.Tree       as Tr
 
 type Word = Text
 type Dictionary = Set Word
@@ -22,17 +25,26 @@ areNeighbours a b = isLength1 $ filter (uncurry (/=)) $ T.zip a b
   where isLength1 [_] = True
         isLength1 _   = False
 
+neighbourMap :: Dictionary -> MS.Map Word (Set Word)
+neighbourMap dict = M.fromList $ map (id &&& neighbours dict) $ S.toList dict
+
 
 type Chain = [Word]
 
-treeFrom :: Dictionary -> Word -> Tree Chain
-treeFrom dict s = Tr.unfoldTree expand (S.delete s dict, [s])
-  where expand :: (Dictionary, Chain) -> (Chain, [(Dictionary, Chain)])
-        expand (d, c) = (c, [(S.delete n d, n : c) | n <- S.toList $ neighbours d $ head c])
+type NeighbourLookup = Word -> Set Word
+dictNeighbourLookup :: Dictionary -> NeighbourLookup
+dictNeighbourLookup dict w = M.findWithDefault S.empty w (neighbourMap dict)
+
+treeFrom :: NeighbourLookup -> Word -> Tree Chain
+treeFrom nl s = Tr.unfoldTree expand (nl, [s])
+  where expand :: (NeighbourLookup, Chain) -> (Chain, [(NeighbourLookup, Chain)])
+        expand (nl', c) = (c, [(neighbourLookupWithout nl' n, n : c) | n <- S.toList $ nl' $ head c])
+        neighbourLookupWithout wrapped exclude = S.delete exclude . wrapped
 
 chains :: Dictionary -> Word -> Word -> [Chain]
-chains dict a b = map reverse $ filter reachesTarget $ concat $ Tr.levels $ treeFrom dict' a
+chains dict a b = map reverse . filter reachesTarget . concat . Tr.levels $ tree
   where dict' = S.filter ((T.length a ==) . T.length) dict
+        tree = treeFrom (dictNeighbourLookup dict') a
         reachesTarget [] = False
         reachesTarget (x:_) = x == b
 
